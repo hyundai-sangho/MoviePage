3e4365c68e8de0adb6fb2fed6bd74b0c9784771f
조상호 <hyundai_sangho@naver.com>
2022-07-31 12:19:12 +0900

    회원가입 페이지 만들기

---
+++ register.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>회원 가입</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+  </head>
+
+  <body>
+    <section class="vh-100 bg-image"
+      style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp');">
+      <div class="mask d-flex align-items-center h-100 gradient-custom-3">
+        <div class="container h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+              <div class="card" style="border-radius: 15px;">
+                <div class="card-body p-5">
+                  <h2 class="text-uppercase text-center mb-5">계정 생성</h2>
+
+                  <form>
+
+                    <div class="form-outline mb-4">
+                      <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
+                      <label class="form-label" for="form3Example1cg">이름</label>
+                    </div>
+
+                    <div class="form-outline mb-4">
+                      <input type="email" id="form3Example3cg" class="form-control form-control-lg" />
+                      <label class="form-label" for="form3Example3cg">이메일</label>
+                    </div>
+
+                    <div class="form-outline mb-4">
+                      <input type="password" id="form3Example4cg" class="form-control form-control-lg" />
+                      <label class="form-label" for="form3Example4cg">비밀번호</label>
+                    </div>
+
+                    <div class="form-outline mb-4">
+                      <input type="password" id="form3Example4cdg" class="form-control form-control-lg" />
+                      <label class="form-label" for="form3Example4cdg">비밀번호 확인</label>
+                    </div>
+
+                    <div class="d-flex justify-content-center">
+                      <button type="button"
+                        class="btn btn-outline-danger btn-block btn-lg gradient-custom-4 text-body">게정
+                        등록</button>
+                    </div>
+                  </form>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+
+</html>
